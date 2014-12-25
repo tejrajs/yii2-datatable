86b5bfaab4bc03579f2257caa4ebe5b4d93ec580
Tej Raj Shrestha <helloteju96@gmail.com>
2014-12-25 11:47:20 +0545

    make first Hello World

--- DataTable.php
+++ DataTable.php
@@ -2,13 +2,25 @@
 
 namespace tejrajs\datatable;
 
+use yii\base\Widget;
+use yii\helpers\Html;
 /**
  * This is just an example.
  */
 class DataTable extends \yii\base\Widget
 {
-    public function run()
-    {
-        return "Hello!";
-    }
+	public $message;
+	
+	public function init(){
+		parent::init();
+		if($this->message===null){
+			$this->message= 'Welcome User';
+		}else{
+			$this->message= 'Welcome '.$this->message;
+		}
+	}
+	
+	public function run(){
+		return Html::encode($this->message);
+	}
 }
